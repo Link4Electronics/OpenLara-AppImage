201491bebba4d6f1f6abbf7fe9793f9f018728f6
Link4Electronics <link4electronics@gmail.com>
2026-02-05 11:50:45 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/openlara
+quick-sharun /usr/bin/OpenLara
 
 # Additional changes can be done in between here
 
