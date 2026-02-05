1d301eefb4654884449d1c0d9364fa5c66a05032
Link4Electronics <link4electronics@gmail.com>
2026-02-05 11:49:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,28 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    libdecor \
+    libx11   \
+    libgl    \
+    libpulse \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package openlara-git
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of OpenLara..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/XProger/OpenLara"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./OpenLara
+echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "$DEVEL_RELEASE" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cd ./OpenLara/src/platform/sdl2
+./build.sh
+mv -v OpenLara /usr/bin/openlara
